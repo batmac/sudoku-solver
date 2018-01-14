5735564edfd5a45d0e1a3406feb6e2ea588bded8
Baptiste Canton <batmac@gmail.com>
2018-01-14 21:59:53 +0100

    Test failed test (travis)

--- test/test.sh
+++ test/test.sh
@@ -44,9 +44,9 @@ if [ "$TEST_PLATFORM" == "qemu-raspbian" ] ; then
 fi
 	
 make
-./sudoku test/false.txt ; r=$?; if [ $r != 255 ]; then echo ERROR $r; exit $r; fi
-./sudoku test/false2.txt; r=$?; if [ $r != 4 ]; then echo ERROR $r; exit $r; fi
-./sudoku test/valid.txt;  r=$?; if [ $r != 0 ]; then echo ERROR $r; exit $r; fi
+./sudoku test/false.txt ; r=$?; if [ $r != 255 ]; then echo ERROR $r; exit -1; fi
+./sudoku test/false2.txt; r=$?; if [ $r != 4 ]; then echo ERROR $r; exit -1; fi
+./sudoku test/valid.txt;  r=$?; if [ $r != 0 ]; then echo ERROR $r; exit -1; fi
 echo
 echo OK
 touch last-test-ok

a9e599411bc66acf20f8b78be1bc35494d5476c1
Baptiste Canton <batmac@gmail.com>
2018-01-14 22:40:29 +0100

    Fix test.sh

--- test/test.sh
+++ test/test.sh
@@ -18,7 +18,7 @@ echo
 echo
 
 [ -f last-test-ok ] && rm last-test-ok
-[ -f env ] && source ./env
+[ -f test-env ] && source ./test-env
 
 if [ "$TEST_PLATFORM" == "qemu-raspbian" ] ; then
 	set -e
@@ -32,7 +32,7 @@ if [ "$TEST_PLATFORM" == "qemu-raspbian" ] ; then
 	pwd
 	time make sync DIR=r FROM="`git rev-parse --show-toplevel`" TO="/tmp/" RSYNC_OPTIONS="--exclude=.git --exclude=qemu-raspbian -v"
 	time make DIR=r
-	echo "CC=$CC" >> r/tmp/*/env
+	echo "CC=$CC" >> r/tmp/*/test-env
 	sudo chroot r /bin/sh -c 'cd tmp/*; pwd; make clean'
 	sudo chroot r /bin/sh -c 'cd tmp/*;make test'
 	if [ -f r/tmp/*/last-test-ok ] ;then

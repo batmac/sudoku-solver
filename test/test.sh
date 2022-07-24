fed4ed8ac0b7f67da9b01667ef5d1993849c4e26
Baptiste Canton <bat@sbz.fr>
2022-07-24 05:58:51 +0200

    add cmake

--- test/test.sh
+++ test/test.sh
@@ -22,40 +22,13 @@ echo
 df -h
 echo
 
-[ -f test/last-test-ok ] && rm test/last-test-ok
-[ -f test/env ] && source ./test/env
-
-if [ "$TEST_PLATFORM" == "qemu-user-raspbian" ] ; then
-	set -e
-	echo "TEST_PLATFORM => $TEST_PLATFORM"
-	unset TEST_PLATFORM
-	cd "`dirname $0`/qemu-user-raspbian"
-	pwd
-	[ -z "$CC" ] && export CC=cc
-	time make sync DIR=chroot FROM="`git rev-parse --show-toplevel`" TO="/tmp/" RSYNC_OPTIONS="--exclude=.git --exclude=qemu-user-raspbian -v"
-	time make DIR=chroot
-	echo "export CC=$CC" >> chroot/tmp/*/test/env
-	sudo chroot chroot /bin/sh -c 'cd /tmp/*; pwd; make clean'
-	sudo chroot chroot /bin/sh -c 'useradd testuser || true'
-	sudo chroot chroot /bin/sh -c 'chown -R testuser /tmp/*'
-	sudo chroot chroot /bin/sh -c 'su testuser -c /bin/sh -c "cd /tmp/*;make test"'
-	if [ -f chroot/tmp/*/test/last-test-ok ] ;then
-		echo "qemu-user-raspbian test succeeded"
-		rm -f chroot/tmp/*/test/last-test-ok
-		exit 0
-	else
-		exit 98
-	fi
-	exit 99
-fi
-	
 time make
-time ./sudoku test/false.txt ; r=$?; if [ $r != 255 ]; then echo ERROR $r; exit -1; fi
-time ./sudoku test/false2.txt; r=$?; if [ $r != 4 ]; then echo ERROR $r; exit -1; fi
-time ./sudoku test/easy.txt;   r=$?; if [ $r != 0 ]; then echo ERROR $r; exit -1; fi
-time ./sudoku test/valid.txt;  r=$?; if [ $r != 0 ]; then echo ERROR $r; exit -1; fi
-time ./sudoku test/empty.txt;  r=$?; if [ $r != 0 ]; then echo ERROR $r; exit -1; fi
-time ./sudoku test/slow.txt;   r=$?; if [ $r != 0 ]; then echo ERROR $r; exit -1; fi
+time ./sudoku test/false.txt ; r=$?; if [ $r != 255 ]; then echo ERROR $r; exit 255; fi
+time ./sudoku test/false2.txt; r=$?; if [ $r != 4 ]; then echo ERROR $r; exit 255; fi
+time ./sudoku test/easy.txt;   r=$?; if [ $r != 0 ]; then echo ERROR $r; exit 255; fi
+time ./sudoku test/valid.txt;  r=$?; if [ $r != 0 ]; then echo ERROR $r; exit 255; fi
+time ./sudoku test/empty.txt;  r=$?; if [ $r != 0 ]; then echo ERROR $r; exit 255; fi
+time ./sudoku test/slow.txt;   r=$?; if [ $r != 0 ]; then echo ERROR $r; exit 255; fi
 echo
 echo OK
 touch test/last-test-ok
